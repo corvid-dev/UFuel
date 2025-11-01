5f18cddedd5f6528219eccfc093d108ae9901657
corvid-dev <67079592+corvid-dev@users.noreply.github.com>
2025-11-01 09:09:54 -0400

    Clean up rebuild script

--- rebuild_launch_docker.ps1
+++ rebuild_launch_docker.ps1
@@ -1,23 +1,25 @@
+# Step 1
 Write-Host "Rebuild UFUEL Docker image..."
 
-# Step 1: Write-Host "Stop Running Containers"
+# Step 2
+Write-Host "Stop Running Containers"
 docker ps -q | ForEach-Object { docker stop $_ } 2>$null
 
-# Step 2: Clean up any stopped containers
-Write-Host "Clean Stopped Containers"
+# Step 3
+Write-Host "Remove Stopped Containers"
 docker ps -aq | ForEach-Object { docker rm -f $_ } 2>$null
 
-# Step 3: Prune unused Docker resources
-Write-Host "Prune Unused Dockers"
+# Step 4
+Write-Host "Prune Unused Dockers Resources"
 docker system prune -af --volumes
 
-# Step 4: Build the Docker image
+# Step 5
 Write-Host "Build New Docker Image"
 docker build -t ufuel . --no-cache --progress=plain
 
-# Step 5: Run the container
+# Step 6
 Write-Host "Start UFuel Container"
 docker run -p 5000:5000 -v "${PWD}:/app" ufuel
 
-# Step 6: Print Completion
+# Step 7
 Write-Host "Rebuild complete. UFUEL is running at http://localhost:5000"
